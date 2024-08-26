74f4660eb45a8d319ab3a173fdbf4e2af6610d2e
xxcosita3czxx <kokosove18@gmail.com>
2024-08-26 21:45:24 +0200

    Handling null

--- js/fetchdata.js
+++ js/fetchdata.js
@@ -8,8 +8,13 @@ function fetchData(url, elementId) {
         })
         .then(data => {
             // Extract the first value from the response object, ignoring the key
-            const value = Object.values(data)[0];
-            document.getElementById(elementId).innerText = value;
+            const values = Object.values(data);
+            if (values.length > 0) {
+                const value = values[0];
+                document.getElementById(elementId).innerText = value;
+            } else {
+                document.getElementById(elementId).innerText = 'No data available';
+            }
         })
         .catch(error => {
             console.error('Error fetching data:', error);
